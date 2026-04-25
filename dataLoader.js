5f430f1a25b4de6e3822fac2ca7e540750110a69
NSPC2025 <saltzmanmusic05@gmail.com>
2026-04-25 12:48:36 +0300

    Update dataLoader.js

--- dataLoader.js
+++ dataLoader.js
@@ -1,9 +1,20 @@
 let cachedArticles = null;
 
 export async function getArticles() {
-  if (cachedArticles) return cachedArticles;
+  try {
+    if (cachedArticles) return cachedArticles;
 
-  const res = await fetch("data/articles.json");
-  cachedArticles = await res.json();
-  return cachedArticles;
+    const res = await fetch("./data/articles.json");
+
+    if (!res.ok) {
+      throw new Error("Failed to load articles.json");
+    }
+
+    cachedArticles = await res.json();
+    return cachedArticles;
+
+  } catch (err) {
+    console.error("Error loading articles:", err);
+    return [];
+  }
 }
